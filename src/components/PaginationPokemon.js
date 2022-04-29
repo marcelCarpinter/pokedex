b857e0e62deb097ce6d870d6918456606160dcae
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2022-04-29 16:44:34 -0300

    Project ends. V 0.0.1

---
+++ src/components/PaginationPokemon.js
@@ -0,0 +1,12 @@
+
+
+function PaginationPokemon(props){
+    let pages = [1,2,3,4,5,6,7,8,9,10];
+    return (
+        <>
+            <Pagination count={10} shape="rounded" ></Pagination>
+        </>
+    )
+}
+
+export default Pagination;
